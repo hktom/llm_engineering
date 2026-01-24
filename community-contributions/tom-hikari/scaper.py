55dd4e6bdaa7cc398c98e66ade5dabe5205c2fb1
hktom <tomprescott1565@gmail.com>
2026-01-24 10:36:56 +0100

    Enhance open_news notebook by adding output messages for environment setup validation and integrating website content fetching functionality. Updated code to display news articles dynamically and improved markdown formatting for better readability.

---
+++ community-contributions/tom-hikari/scaper.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+import requests
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+}
+
+
+def fetch_website_contents(url):
+    """
+    Return the title and contents of the website at the given url;
+    truncate to 2,000 characters as a sensible limit
+    """
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    titles = []
+
+    for level in range(1, 3):
+        title_level = soup.find_all(f"h{level}")
+
+        if len(title_level) == 0:
+            continue
+
+        title = [t.get_text() for t in title_level]
+        titles.extend(title)
+
+    link = soup.find_all("a")
+    link_text = [l.get("href") for l in link]
+    link_texts = "\n".join(link_text)
+
+    return {"titles": titles, "links": link_texts}
